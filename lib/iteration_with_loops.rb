2c10ce79e39c083092a26f137244c408180e1056
logicfirst <slee@logicfirstllc.com>
2020-11-10 16:14:51 -0500

    commit tree ac3f9b81e6004c19fc1e3c3a371775d5c25963fa

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,24 +1,20 @@
 def find_min_in_nested_arrays(src)
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
-  
-  row_index = 0 
-  daily_min_temp = [ ]
+  low_temps = []
+  row_index = 0
 
-  while row_index < src.count do 
+  while row_index < src.count do
     element_index = 0
-    hourly_temp = src[row_index][element_index]
+    low_temp = src[row_index][element_index]
     while element_index < src[row_index].count do
-    
-      if 
-        src[row_index][element_index] < hourly_temp
-        hourly_temp = src[row_index][element_index]
+      if src[row_index][element_index] <= low_temp
+        low_temp = src[row_index][element_index]
       end
-      element_index += 1 
+      element_index += 1
     end
-    daily_min_temp << hourly_temp
+    low_temps << low_temp
     row_index += 1
-  end  
-  daily_min_temp  
-
+  end
+  low_temps
 end
